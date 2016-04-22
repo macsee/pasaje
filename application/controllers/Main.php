a470f82d8b7d0e14b963da91f4a69ab3acdfbdc0
macsee <macsee@gmail.com>
2016-04-21 22:11:00 -0300

    Modificacion

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -476,7 +476,7 @@ class Main extends CI_Controller {
 	public function get_nota($id,$fecha="")
 	{
 		if ($fecha != "") {
-			$notas = $this->main_model->get_data('notas', null, array('fecha' => $fecha), array("last_update","desc"));
+			$notas = $this->main_model->get_data('notas', null, array('fecha' => $fecha), array("last_update","asen"));
 
 			if ($notas != null) {
 				foreach ($notas as $key => $value) {
